fc442a8ed0abee70d16606b565f09f3a2781b6b2
Herrington Darkholme <nonamesheep1@gmail.com>
2016-10-03 21:03:54 +0800

    add store example

--- app.vue
+++ app.vue
@@ -1,12 +1,31 @@
 <template>
-<h1 @click="hello">hello {{name}}</h1>
+<h1 @click="hello">
+  hello {{name}} for {{i}} times
+  <button @click="add">+</button>
+  <button @click="sub">-</button>
+</h1>
+
 </template>
 
 <script>
-import {Vue, Component} from 'av-ts'
+import { Vue, Component } from 'av-ts'
+import { create, getHelper, Vuex } from 'kilimanjaro'
+
+var store = create({count: 0})
+  .getter('count', s => s.count)
+  .mutation('increment', s => () => s.count++)
+  .mutation('decrement', s => () => s.count--)
+  .done()
+
+var { getters, commit } = getHelper(store)
+
 @Component
 export default class App extends Vue {
   name = 'Rem'
+  @Vuex i = getters('count')
+  @Vuex add = commit('increment')
+  @Vuex sub = commit('decrement')
+
   hello() {
     alert('hello ' + this.name)
   }
